c30b80cc3aa98034c36efd3492d22dc28aac829f
Egide T. <tumugide@gmail.com>
2024-10-30 12:14:38 +0200

    =default up to migrations

---
+++ PokemonReviewApp/Model/Country.cs
@@ -0,0 +1,8 @@
+﻿namespace PokemonReviewApp.Model;
+
+public class Country
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public ICollection<Owner> Owners { get; set; }
+}
